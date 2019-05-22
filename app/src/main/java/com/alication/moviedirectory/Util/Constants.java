77365f4d5efbba5e58264c262f06d0b713ca65c0
Loonixas <loonixas@gmail.com>
2019-05-22 14:46:30 +0300

    Pilnai veikiantis

---
+++ app/src/main/java/com/alication/moviedirectory/Util/Constants.java
@@ -0,0 +1,9 @@
+package com.alication.moviedirectory.Util;
+
+public class Constants {
+
+    public static final String URL_LEFT = "http://www.omdbapi.com/?s=";
+    public static final String URL = "http://www.omdbapi.com/?i=";
+    public static final String API_KEY = "&apikey=b1dbc7be";
+    public static final String URL_RIGHT = "&plot=full";
+}
